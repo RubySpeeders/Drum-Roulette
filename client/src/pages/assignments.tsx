aa69879db0d55157561239167f500c3f6b3c9806
Sarah Peters <sarahnpeters@gmail.com>
2023-04-12 20:21:25 -0500

    creating assignments page, clicking on button goes to assignments page. initial dump of code into landing page

---
+++ client/src/pages/assignments.tsx
@@ -0,0 +1,29 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
+import { useState } from 'react';
+
+interface Assignment {
+  name: string;
+  instrument: string;
+}
+export default function Assignments() {
+  const [assignments, setAssignments] = useState<Assignment[]>([
+    { name: 'Chad', instrument: 'bass drum' },
+  ]);
+  return (
+    <div>
+      {assignments.map((assignment) => {
+        return (
+          <Card
+            style={{ border: 'solid pink', margin: '.25em', width: '50em' }}
+            key={assignment.name}
+          >
+            <CardContent>
+              <Typography>Name: {assignment.name}</Typography>
+              <Typography>Instrument: {assignment.instrument}</Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
--- client/src/pages/choosePlayers.tsx
+++ client/src/pages/choosePlayers.tsx
@@ -1,3 +1,153 @@
+import { useState } from 'react';
+import { shuffle } from 'lodash';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import { useRouter } from 'next/router';
+
 export default function ChoosePlayers() {
-  return <div>Choose Players</div>;
+  interface Name {
+    name: string;
+    selected: boolean;
+  }
+  interface Instrument {
+    instrument: string;
+    selected: boolean;
+  }
+  interface Assignment {
+    name: string;
+    instrument: string;
+  }
+
+  const [instruments, setInstruments] = useState<Instrument[]>([
+    { instrument: 'bass drum', selected: false },
+    { instrument: 'snare', selected: false },
+    { instrument: 'triangle', selected: false },
+  ]);
+
+  const [names, setNames] = useState<Name[]>([
+    { name: 'Chad', selected: false },
+    { name: 'Nick', selected: false },
+    { name: 'Brad', selected: false },
+  ]);
+  const [assignments, setAssignments] = useState<Assignment[]>([
+    { name: 'Chad', instrument: 'bass drum' },
+  ]);
+
+  const selectedEqual =
+    instruments.filter((instrument) => instrument.selected).length ===
+    names.filter((name) => name.selected).length;
+
+  const handleClickName = (item: Name) => {
+    const nextName = names.map((name) => {
+      if (name.name === item.name) {
+        return { name: item.name, selected: !item.selected };
+      } else {
+        return name;
+      }
+    });
+    setNames(nextName);
+  };
+
+  const handleClickInstrument = (item: Instrument) => {
+    const nextInstrument = instruments.map((instrument) => {
+      if (instrument.instrument === item.instrument) {
+        return { instrument: item.instrument, selected: !item.selected };
+      } else {
+        return instrument;
+      }
+    });
+    setInstruments(nextInstrument);
+  };
+
+  const router = useRouter();
+
+  const assign = (names: Name[], instruments: Instrument[]) => {
+    // const assignment: Assignment = { name: "", instrument: "" }
+    const shuffledNames = shuffle(names.filter((name) => name.selected));
+    const shuffledInstruments = shuffle(
+      instruments.filter((instrument) => instrument.selected)
+    );
+    const nextAssignment = shuffledNames.map((name, i) => {
+      return { name: name.name, instrument: shuffledInstruments[i].instrument };
+    });
+    setAssignments(nextAssignment);
+  };
+
+  return (
+    <div>
+      <div>Choose Players</div>
+      <h2>Names</h2>
+      {names
+        .filter((name) => !name.selected)
+        .map((name) => (
+          <Card
+            style={{ border: 'solid blue', margin: '.25em', width: '50em' }}
+            key={name.name}
+            onClick={() => handleClickName(name)}
+          >
+            <CardContent>
+              <Typography>{name.name}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      <div>
+        <p>Selected Names</p>
+        {names
+          .filter((name) => name.selected)
+          .map((name) => (
+            <Card
+              style={{ border: 'solid blue', margin: '.25em', width: '50em' }}
+              key={name.name}
+              onClick={() => handleClickName(name)}
+            >
+              <CardContent>
+                <Typography>{name.name}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
+      <div>
+        <h2>Instruments</h2>
+        {instruments
+          .filter((instrument) => !instrument.selected)
+          .map((instrument) => (
+            <Card
+              style={{ border: 'solid red', margin: '.25em', width: '50em' }}
+              key={instrument.instrument}
+              onClick={() => handleClickInstrument(instrument)}
+            >
+              <CardContent>
+                <Typography>{instrument.instrument}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
+      <Typography>Selected Instruments</Typography>
+      {instruments
+        .filter((instrument) => instrument.selected)
+        .map((instrument) => (
+          <Card
+            style={{ border: 'solid red', margin: '.25em', width: '50em' }}
+            key={instrument.instrument}
+            onClick={() => handleClickInstrument(instrument)}
+          >
+            <CardContent>
+              <Typography>{instrument.instrument}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          // assign(names, instruments);
+          router.push({
+            pathname: '/assignments',
+            query: { object: JSON.stringify(assignments) },
+          });
+        }}
+        disabled={!selectedEqual}
+      >
+        Give me assignments!
+      </button>
+    </div>
+  );
 }
