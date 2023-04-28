47f6a5d29956028145dc465e76f28f2756b30e27
Sarah Peters <sarahpeters@Bradleys-MacBook-Air.local>
2023-04-28 15:44:11 -0400

    adding classNames package, some cleanup on naming

--- client/src/pages/choosePlayers.tsx
+++ client/src/pages/choosePlayers.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { shuffle } from 'lodash';
 import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 
 const useStyles = makeStyles({
   card: {
@@ -14,12 +15,12 @@ const useStyles = makeStyles({
 });
 
 export default function ChoosePlayers() {
-  interface Name {
+  interface Musician {
     name: string;
     selected: boolean;
   }
   interface Instrument {
-    instrument: string;
+    name: string;
     selected: boolean;
   }
   interface Assignment {
@@ -29,12 +30,12 @@ export default function ChoosePlayers() {
 
   const classes = useStyles();
   const [instruments, setInstruments] = useState<Instrument[]>([
-    { instrument: 'bass drum', selected: false },
-    { instrument: 'snare', selected: false },
-    { instrument: 'triangle', selected: false },
+    { name: 'bass drum', selected: false },
+    { name: 'snare', selected: false },
+    { name: 'triangle', selected: false },
   ]);
 
-  const [musicians, setMusicians] = useState<Name[]>([
+  const [musicians, setMusicians] = useState<Musician[]>([
     { name: 'Chad', selected: false },
     { name: 'Nick', selected: false },
     { name: 'Brad', selected: false },
@@ -47,21 +48,21 @@ export default function ChoosePlayers() {
     instruments.filter((instrument) => instrument.selected).length ===
     musicians.filter((name) => name.selected).length;
 
-  const handleClickMusician = (item: Name) => {
-    const nextName = musicians.map((name) => {
-      if (name.name === item.name) {
+  const handleClickMusician = (item: Musician) => {
+    const nextMusician = musicians.map((musician) => {
+      if (musician.name === item.name) {
         return { name: item.name, selected: !item.selected };
       } else {
-        return name;
+        return musician;
       }
     });
-    setMusicians(nextName);
+    setMusicians(nextMusician);
   };
 
   const handleClickInstrument = (item: Instrument) => {
     const nextInstrument = instruments.map((instrument) => {
-      if (instrument.instrument === item.instrument) {
-        return { instrument: item.instrument, selected: !item.selected };
+      if (instrument.name === item.name) {
+        return { name: item.name, selected: !item.selected };
       } else {
         return instrument;
       }
@@ -71,14 +72,14 @@ export default function ChoosePlayers() {
 
   const router = useRouter();
 
-  const assign = (names: Name[], instruments: Instrument[]) => {
+  const assign = (names: Musician[], instruments: Instrument[]) => {
     // const assignment: Assignment = { name: "", instrument: "" }
     const shuffledNames = shuffle(names.filter((name) => name.selected));
     const shuffledInstruments = shuffle(
       instruments.filter((instrument) => instrument.selected)
     );
     const nextAssignment = shuffledNames.map((name, i) => {
-      return { name: name.name, instrument: shuffledInstruments[i].instrument };
+      return { name: name.name, instrument: shuffledInstruments[i].name };
     });
     setAssignments(nextAssignment);
   };
@@ -87,9 +88,9 @@ export default function ChoosePlayers() {
     <div>
       <div>Choose Players</div>
       <h2>Names</h2>
-      {musicians.map((musician: Name) => (
+      {musicians.map((musician: Musician) => (
         <Card
-          className={`${classes.card} ${musician.selected && classes.selected}`}
+          className={classNames(classes.card, {[classes.selected]: musician.selected})}
           key={musician.name}
           onClick={() => handleClickMusician(musician)}
         >
@@ -105,11 +106,11 @@ export default function ChoosePlayers() {
             className={`${classes.card} ${
               instrument.selected && classes.selected
             }`}
-            key={instrument.instrument}
+            key={instrument.name}
             onClick={() => handleClickInstrument(instrument)}
           >
             <CardContent>
-              <Typography>{instrument.instrument}</Typography>
+              <Typography>{instrument.name}</Typography>
             </CardContent>
           </Card>
         ))}
