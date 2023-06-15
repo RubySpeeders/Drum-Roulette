acf66b298341dd3d168b84d03b38a1c5922a3fd1
Sarah Peters <sarahnpeters@gmail.com>
2023-06-15 16:07:39 -0400

    adding drum favicon, playing with css to wrap musicians and instruments across

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- client/src/pages/choosePlayers.tsx
+++ client/src/pages/choosePlayers.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import { shuffle } from 'lodash';
-import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
-import { useRouter } from 'next/router';
-import classNames from 'classnames';
+import { useState } from "react";
+import { shuffle } from "lodash";
+import { Card, CardContent, Typography, makeStyles } from "@material-ui/core";
+import { useRouter } from "next/router";
+import classNames from "classnames";
 
 const useStyles = makeStyles({
   card: {
-    margin: '.25em',
-    width: '50em',
+    margin: ".25em",
+    // width: "30em",
   },
   selected: {
-    border: 'solid #37A8FA',
+    border: "solid #37A8FA",
   },
 });
 
@@ -30,18 +30,21 @@ export default function ChoosePlayers() {
 
   const classes = useStyles();
   const [instruments, setInstruments] = useState<Instrument[]>([
-    { name: 'bass drum', selected: false },
-    { name: 'snare', selected: false },
-    { name: 'triangle', selected: false },
+    { name: "bass drum", selected: false },
+    { name: "snare", selected: false },
+    { name: "triangle", selected: false },
   ]);
 
   const [musicians, setMusicians] = useState<Musician[]>([
-    { name: 'Chad', selected: false },
-    { name: 'Nick', selected: false },
-    { name: 'Brad', selected: false },
+    { name: "Chad", selected: false },
+    { name: "Nick", selected: false },
+    { name: "Brad", selected: false },
+    { name: "Chad", selected: false },
+    { name: "Nick", selected: false },
+    { name: "Brad", selected: false },
   ]);
   const [assignments, setAssignments] = useState<Assignment[]>([
-    { name: 'Chad', instrument: 'bass drum' },
+    { name: "Chad", instrument: "bass drum" },
   ]);
 
   const selectedEqual =
@@ -88,39 +91,45 @@ export default function ChoosePlayers() {
     <div>
       <div>Choose Players</div>
       <h2>Names</h2>
-      {musicians.map((musician: Musician) => (
-        <Card
-          className={classNames(classes.card, {[classes.selected]: musician.selected})}
-          key={musician.name}
-          onClick={() => handleClickMusician(musician)}
-        >
-          <CardContent>
-            <Typography>{musician.name}</Typography>
-          </CardContent>
-        </Card>
-      ))}
-      <div>
-        <h2>Instruments</h2>
-        {instruments.map((instrument) => (
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {musicians.map((musician: Musician) => (
           <Card
-            className={`${classes.card} ${
-              instrument.selected && classes.selected
-            }`}
-            key={instrument.name}
-            onClick={() => handleClickInstrument(instrument)}
+            className={classNames(classes.card, {
+              [classes.selected]: musician.selected,
+            })}
+            key={musician.name}
+            onClick={() => handleClickMusician(musician)}
           >
             <CardContent>
-              <Typography>{instrument.name}</Typography>
+              <Typography>{musician.name}</Typography>
             </CardContent>
           </Card>
         ))}
+      </div>
+      <div>
+        <h2>Instruments</h2>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {instruments.map((instrument) => (
+            <Card
+              className={`${classes.card} ${
+                instrument.selected && classes.selected
+              }`}
+              key={instrument.name}
+              onClick={() => handleClickInstrument(instrument)}
+            >
+              <CardContent>
+                <Typography>{instrument.name}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       </div>
       <button
         onClick={(e) => {
           e.preventDefault();
           // assign(names, instruments);
           router.push({
-            pathname: '/assignments',
+            pathname: "/assignments",
             query: { object: JSON.stringify(assignments) },
           });
         }}
