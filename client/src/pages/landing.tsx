88196afee8cfee8e4af18f51f283820d0892339c
Sarah Peters <sarahnpeters@gmail.com>
2023-06-19 23:12:08 -0400

    adding to landing page

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import navyLogo from "../../public/assets/images/USNavy-Band-Logo.png";
+import { Typography } from "@material-ui/core";
 
 export default function Assignments() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", width: "80vw" }}>
       <h1>Musician Personnel Assignment</h1>
+      <h2>randomly generate assignments with one click</h2>
       <h3>Please Select Your Branch</h3>
       <Link href="/selectPlayers">
-        <Image src={navyLogo} alt="navyLogo" />
+        <Image src={navyLogo} alt="navyLogo" height={100} width={100} />
+        <Typography>US Navy Band</Typography>
       </Link>
     </div>
   );
