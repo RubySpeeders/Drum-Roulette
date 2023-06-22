047fe9c9cc773fe0cad7303e8930be9695b278f0
Sarah Peters <sarahnpeters@gmail.com>
2023-06-21 20:47:40 -0400

    initial migration of mui 4->5

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
+import { ThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+
+const theme = createMuiTheme();
+
+const useStyles = makeStyles((theme) => {
+  root: {
+    // some CSS that accesses the theme
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) jssStyles.parentElement?.removeChild(jssStyles);
-  }, []);
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
