0e8ce121fdfc0b1a05ad82d66f37f85fcaaf9acf
Sarah Peters <sarahnpeters@gmail.com>
2023-08-02 08:51:03 -0500

    cleaning up the style object

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -3,22 +3,14 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     background: {
-      default: "#ff69b4", // Replace with your desired background color
+      default: "#515A6B", // Replace with your desired background color
     },
     primary: {
-      // light: ??
       main: "#B2B4CB",
-      // dark: ??
-      // contrastText: ??
     },
     secondary: {
-      // light: ??
       main: "#515A6B",
-      // dark: ??
-      //   contrastText: ??,
     },
-    // Used by `getContrastText()` to maximize the contrast between the background and the text.
-    contrastThreshold: 4.5,
   },
 });
 
