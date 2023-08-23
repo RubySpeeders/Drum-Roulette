bf34bdff54a83d08ca17ef0a261737dddefe2f90
Charlotte Kies <chopekies@gmail.com>
2023-08-22 22:04:38 -0400

    Added configurations to make website display more like an App when added to mobile device home screens

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -9,6 +9,30 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+
+          {/* Configurations to make site to appear like an app when pinned to phone home screen */}
+
+          {/* Configuration file "manifest" configures site to appear app-like when added to and launched from Android homescreen */}
+          <link rel="manifest" href="/manifest.json" />
+
+          {/* Apple-specific configurations to make site appear app-like when added to and launched from iOS mobile device homescreen */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/assets/images/favicon.ico"
+          />
+
+          {/* Makes website opened from iPhone home screen appear more like an app, no browser URL at the top of the screen e.g. */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+
+          {/* Gives the "app" a name when added to an iPhone's home screen */}
+          <meta name="apple-mobile-web-app-title" content="Drum Roulette" />
+
+          {/* Colors the status bar (battery, wifi, data) white */}
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
         </Head>
         <body>
           <Main />
