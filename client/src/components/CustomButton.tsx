9d4e9283a242e5e88f86d1f92c0f0406d8f8b46e
Angelo Quail <angeloquail@angelos-air.mynetworksettings.com>
2023-09-28 20:08:08 -0400

    refactored styling for disabled button

--- client/src/components/CustomButton.tsx
+++ client/src/components/CustomButton.tsx
@@ -15,15 +15,7 @@ interface Props {
     | "warning";
   variant?: "text" | "outlined" | "contained";
   disabled?: boolean | undefined;
-  state?: "inactive" | undefined;
 }
-
-const useStyles = makeStyles(() => ({
-  inactive: {
-    backgroundColor: "#E9E5F3",
-    "&:hover": { backgroundColor: "#E9E5F3", cursor: "not-allowed" },
-  },
-}));
 
 export const CustomButton = ({
   children,
@@ -31,17 +23,19 @@ export const CustomButton = ({
   color = "primary",
   variant = "contained",
   disabled = false,
-  state = undefined,
 }: Props) => {
-  const { inactive } = useStyles();
-
   return (
     <Button
-      className={state === "inactive" ? inactive : ""}
       sx={{
         borderRadius: "3.75em",
         fontSize: "1rem",
         padding: ".75em 4.75em",
+        "&.Mui-disabled": {
+          backgroundColor: "#E9E5F3",
+          color: "white",
+          cursor: "not-allowed",
+          pointerEvents: "auto",
+        },
       }}
       color={color}
       variant={variant}
