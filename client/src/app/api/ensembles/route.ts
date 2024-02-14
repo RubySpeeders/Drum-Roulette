abb392a27e75a77772635506b5386904294f521c
RubySpeeders <sarahnpeters@gmail.com>
2024-02-14 08:30:55 -0500

    adding routes for musicians, ensembles, branch, and instrument

---
+++ client/src/app/api/ensembles/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+
+import pkg from "pg";
+const { Client } = pkg;
+
+export const GET = async () => {
+  // Use environment variables for database configuration
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER, // Use environment variable or provide a default
+    password: process.env.DB_PW, // Use environment variable or provide a default
+    port: 5432, // Default PostgreSQL port
+    ssl: {
+      rejectUnauthorized: false, // Set to true if you want to reject unauthorized certificates
+    },
+  };
+
+  try {
+    // Create a new PostgreSQL client
+    const client = new Client(dbConfig);
+
+    // Connect to the database
+    await client.connect();
+
+    // Your SQL query
+    const query = "SELECT * FROM ensemble";
+
+    // Execute the query
+    const result = await client.query(query);
+
+    // Close the client connection
+    await client.end();
+
+    return NextResponse.json(result.rows);
+  } catch (error) {
+    console.error("Error:", error);
+
+    return NextResponse.json({
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "An error occurred",
+        // error: error.message,
+      }),
+    });
+  }
+};
