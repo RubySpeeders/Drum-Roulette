ce930d92344d099fe0c8f33c0126a2774ba1bda0
Angelo Quail <quailangelojunk@gmail.com>
2024-03-28 14:51:11 -0400

    updating tsconfig, branchesData refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Landing from "@/components/Landing";
 
 export default async function Home() {
   const branchesData = await getBranches();
+  console.log(branchesData);
 
-  return <Landing branches={branchesData.branches} />;
+  return <Landing branches={branchesData} />;
 }
